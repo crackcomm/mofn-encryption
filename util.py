40b2029970f10810975c474f61eb69ccd1d46649
Łukasz Kurowski <crackcomm@gmail.com>
2019-04-08 01:41:08 +0200

    cleanup util

--- util.py
+++ util.py
@@ -17,14 +17,6 @@ def public_bytes(private_key):
         format=serialization.PublicFormat.Raw)
 
 
-def b64_encode_private(private_key):
-    return base64.b64encode(private_key.private_bytes(
-        encoding=serialization.Encoding.Raw,
-        format=serialization.PrivateFormat.Raw,
-        encryption_algorithm=serialization.NoEncryption()
-    ))
-
-
 def exchange_key(private_key, public_key):
     return X25519PrivateKey.from_private_bytes(private_key).exchange(public_key)
 
